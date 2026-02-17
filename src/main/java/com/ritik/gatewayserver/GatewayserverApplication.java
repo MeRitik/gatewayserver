21548bb00cbd67c2408ac7d59f69a263478c729a
Ritik Ranjan <ritikranjan999@gmail.com>
2026-02-17 22:57:40 +0530

    Implemented Custom Routing and disabled automatic discovery locator

--- src/main/java/com/ritik/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/ritik/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,38 @@ package com.ritik.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
 
+    @Bean
+    public RouteLocator bankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(predicateSpec ->
+                        predicateSpec.path("/bank/accounts/**")
+                                .filters(filter -> filter.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://ACCOUNTS")
+
+                )
+                .route(predicateSpec ->
+                        predicateSpec.path("/bank/loans/**")
+                                .filters(filter -> filter.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://LOANS")
+
+                )
+                .route(predicateSpec ->
+                        predicateSpec.path("/bank/cards/**")
+                                .filters(filter -> filter.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://CARDS")
+
+                )
+                .build();
+    }
 }
