d8e4a82459f246f1888c3a21b2b45cf96614bdd2
Ritik Ranjan <ritikranjan999@gmail.com>
2026-02-17 22:24:09 +0530

    Added Gateway server for bank microservice application

---
+++ src/test/java/com/ritik/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritik.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
